0caecba3c04d1f893872b2655c6c822458e9c92b
Robotechnic <robotechnic.developeur@gmail.com>
2020-09-02 19:56:43 +0200

    finished render thread

--- src/renderthread.cpp
+++ src/renderthread.cpp
@@ -4,3 +4,76 @@ renderThread::renderThread()
 {
 
 }
+
+void renderThread::startRender(int zoom, int centerX, int centerY, double escapeRadius, int xMin, int xMax, int yMin, int yMax){
+    this->zoom = zoom;
+    this->centerX = centerX;
+    this->centerY = centerY;
+    this->xMin = xMin;
+    this->xMax = xMax;
+    this->yMin = yMin;
+    this->yMax = yMax;
+    this->escapeRadius = escapeRadius;
+    this->render.allocate(xMax-xMin, yMax-yMin, OF_IMAGE_COLOR);
+
+    this->startThread();
+}
+
+void renderThread::threadedFunction(){
+    //the render of the image
+    for(int x=xMin; x<xMax; x++)
+    {
+        for(int y=yMin; y<yMax; y++)
+        {
+            this->render.setColor(x,y,this->renderPoint(x,y));
+        }
+    }
+    this->stopThread();
+}
+
+ofColor renderThread::renderPoint(int x, int y){
+    //render a spécific point of the image
+    c_x = (x - (widthImg)/2)/zoom/1000+centerX;
+    c_y = (y-heightImg/2)/zoom/1000+centerY;
+    z_x = c_x;
+    z_y = c_y;
+    iter = 0;
+    ofColor color;
+    do
+    {
+
+    newZX = z_x;
+    //mandelbrot *3
+    //z_x = z_x*z_x*z_x*z_x - 6 * z_x * z_x * z_y * z_y + + z_y*z_y*z_y*z_y + c_x;
+    //z_y = 4 * newZX * newZX * newZX * z_y - 4 * z_y * z_y * z_y * newZX + c_y;
+
+    //mandelbrat normal
+    /*z_x = z_x*z_x - z_y * z_y + c_x;
+    z_y = newZX*z_y*2 + c_y;*/
+
+    z_x = z_x*z_x - z_y * z_y  + c_x;
+    z_y = newZX*z_y*2 + c_y;
+
+    //fractale trigo hyperbolique
+    //z_x =sinh(z_x)*cos(z_y)+(c_x*c_x-c_y*c_y)/(pow(c_x,10)+pow(c_y,10));
+    //z_y = cos(z_x)*sinh(z_y)-(2*c_x*c_y)/(pow(c_x,10)+pow(c_y,10));
+    if (iter >= iterMax) {
+        //ofLogError() << "Debug1 " << iter;
+        color = ofColor::fromHsb(0,0,0);
+        break;
+    }
+    if (z_x*z_x + z_y*z_y > escapeRadius) {
+        //ofLogError() << "Debug2 " << iter;
+     color = ofColor::fromHsb((iter*1)%255, 255, 255);
+      break;
+    }
+    iter += 1;
+
+    } while (true);
+
+    return color;
+}
+
+ofImage renderThread::getRender(){
+    return this->render;
+}
--- src/renderthread.h
+++ src/renderthread.h
@@ -2,11 +2,31 @@
 #define RENDERTHREAD_H
 
 #include <ofThread.h>
+#include <ofColor.h>
+#include <ofImage.h>
 
 class renderThread : public ofThread
 {
 public:
     renderThread();
+    void threadedFunction();
+    void startRender(int zoom, int centerX, int centerY, double escapeRadius, int xMin, int xMax, int yMin, int yMax);
+    ofImage getRender();
+
+private:
+    int iterMax, width, height;
+    double c_x;
+    double c_y;
+    double z_x;
+    double z_y;
+    int iter;
+    double newZX;
+    double widthg;
+    double widthImg,heightImg;
+    double escapeRadius;
+    int zoom,  centerX, centerY, yMin, yMax, xMin, xMax;
+    ofColor renderPoint(int x, int y);
+    ofImage render;
 };
 
 #endif // RENDERTHREAD_H
